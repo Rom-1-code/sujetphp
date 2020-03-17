042ce3462d8bcad93e9553ff77f716929e21a47c
FLEMAL <55393709+Rom-1-code@users.noreply.github.com>
2020-03-17 10:51:36 +0100

    com

--- index.php
+++ index.php
@@ -11,14 +11,14 @@
 
 <body>
     <?php
-    try {
+    try { // try catch pour empecher une eventuel erreur de connexion à la bdd
         $Base = new PDO('mysql:host=localhost; dbname=sujetphp; charset=utf8', 'root', 'root');
         $AfficherUser = $Base->query('SELECT * From user');
     } catch (Exception $e) {
         echo "Erreur ";
     }
 
-    while ($afficher = $AfficherUser->fetch()) {
+    while ($afficher = $AfficherUser->fetch()) { // boucle qui affiche les utilisateur tant qu'il existe dans la base
     ?>
         <table border=1>
             <tr>
@@ -34,7 +34,7 @@
     }
     ?>
     <?php
-    if (!isset($_SESSION['Login'])) {
+    if (!isset($_SESSION['Login'])) { // si aucune session n'existe on affiche le formulaire de connexion
     ?>
         <h1>Connexion</h1>
         <form action="index.php" method="POST">
@@ -51,7 +51,7 @@
     }
     ?>
     <?php //PHP pour la connection
-    try {
+    try { // // try catch pour empecher une eventuel erreur de connexion à la bdd
         $Base = new PDO('mysql:host=localhost; dbname=sujetphp; charset=utf8', 'root', 'root');
     } catch (Exception $e) {
         echo "Erreur ";
@@ -60,24 +60,24 @@
     if (isset($_POST['Login']) && isset($_POST['Mdp'])) {
         $LoginConnect = $_POST['Login'];
         $MdpConnect = $_POST['Mdp'];
-        $requser = $Base->prepare("SELECT * FROM user WHERE `Login` = ? AND Mdp = ?");
+        $requser = $Base->prepare("SELECT * FROM user WHERE `Login` = ? AND Mdp = ?"); // requete pour verifier que l'utilisateur correspond bien avec les données dans la bdd
         $requser->execute(array($LoginConnect, $MdpConnect));
         $Loginexist = $requser->rowCount();
         if ($Loginexist == 1) {
             $Logininfo = $requser->fetch();
-            $_SESSION['Login'] = $Logininfo['Login'];
+            $_SESSION['Login'] = $Logininfo['Login']; // creation d'une variable de session pour que l'utilisateur sois afficher meme une fois la page recharger
             echo "<p> Vous être connecté en tant que " . $Logininfo['Login'] . ". </p>";
-        } else{
+        } else {
             echo "<p>Identifiant ou mot de passe incorrect !</p>";
         }
     }
-    
-        if (!isset($_POST['deconnexion'])) {
-            session_destroy();
-        } else {
-            echo "Au revoir";
-        }
-    
+
+    if (!isset($_POST['deconnexion'])) { // si l'utilisateur appuie sur le bouton de deconnexion la session ce detruitf
+        session_destroy();
+    } else {
+        echo "Au revoir";
+    }
+
     ?>
 
 
--- user.php
+++ user.php
@@ -1,13 +1,14 @@
 <?php
 
 class user{
-    
+
     //Propiétés
 
     private $_idUser;
     private $_Login;
     private $_Mdp;
 
+    //Constructeur
     public function __construct($id,$Login,$Mdp)
     {
         $this->_idUser = $id;
